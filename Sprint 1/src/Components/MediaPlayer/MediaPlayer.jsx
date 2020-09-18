d1e00010d779b7f17f6f0da6845a5a31ceffb7b7
Sean Pollock <seanpollock@Seans-MacBook-Air-2.local>
2020-09-18 19:03:35 -0400

    jsx all done, on to styling

--- Sprint 1/src/Components/MediaPlayer/MediaPlayer.jsx
+++ Sprint 1/src/Components/MediaPlayer/MediaPlayer.jsx
@@ -2,25 +2,23 @@ import React from 'react';
 import Play from '../../4.0 - Assets/Icons/PNG/Icon-play.png';
 import FullScreen from '../../4.0 - Assets/Icons/PNG/Icon-fullscreen.png';
 import Volume from '../../4.0 - Assets/Icons/PNG/Icon-volume.png';
-import MainVid from '../../4.0 - Assets/Images/video-list-0.jpg';
 
-class MediaPlayer extends React.Component {
-    state = {mainVid: MainVid}
-    render() {
-        return (
-            <div className="media">
-                <div>
-                    <MainVid VidOne={this.state.mainVid}/>
-                </div>
-                <button className="media__leftButton"><img className="media__leftButtonIcon"src={Play} alt=""/></button>
-                <button className="media__centreButton">
-                    <hr/>
-                    0:00/0:42
-                </button>
-                <button className="media__rightButton"><img className="media__rightButtonIcon" src={FullScreen} alt=""/><img className="media__rightButtonIcon" src={Volume} alt=""/></button>
+export default function MediaPlayer(props) {
+    return (
+        <div className="media">
+            <div>
+                <video className="media__video"
+                src={props.videoSrc} 
+                alt={props.altText}
+                />
             </div>
-        );
-    } 
+            <button className="media__leftButton"><img className="media__leftButtonIcon"src={Play} alt=""/></button>
+            <button className="media__centreButton">
+                <hr/>
+                0:00/0:42
+            </button>
+            <button className="media__rightButton"><img className="media__rightButtonIcon" src={FullScreen} alt=""/><img className="media__rightButtonIcon" src={Volume} alt=""/></button>
+        </div>
+    )
 }
 
-export default MediaPlayer;
--- Sprint 1/src/Components/Video Gallery/VideoGallery.jsx
+++ Sprint 1/src/Components/Video Gallery/VideoGallery.jsx
@@ -1,73 +1,19 @@
 import React from 'react';
 
-class VideoGallery extends React.Component {
-    state = { vidArr: [
-        {
-            img:'',
-            title: 'Become A Travel Pro In One Easy Lesson',
-            author: 'Bernice Lambert',
-            id: 1
-        },
-        {
-            img: '',
-            title: 'Les Houches The Hidden Gem Of The Chamonix',
-            author: 'Bernard Patrick',
-            id: 2
-        },
-        {
-            img: '',
-            title: 'Travel Health Useful Medical Information For',
-            author: 'Lizzie Burton',
-            id: 3
-        },
-        {
-            img: '',
-            title: 'Cheap Airline Tickets Great Ways To Save',
-            author: 'Emily Harper',
-            id: 4            
-        },
-        {
-            img: '',
-            title: 'Take A Romantic Break In A Boutique Hotel',
-            author: 'Ethan Owen',
-            id: 5
-        },
-        {
-            img: '',
-            title: 'Choose The Perfect Accomodations',
-            author: 'Lydia Perez',
-            id: 6
-        },
-        {
-            img: '',
-            title: 'Cruising Destination Ideas',
-            author: 'Timothy Austin',
-            id: 7
-        },
-        {
-            img: '',
-            title: 'Train Travel On Track For Safety',
-            author: 'Scotty Cranmer',
-            id: 8
-        },
-    ]}
-        render() {
-            return (
-                this.state.vidArr.map((item) => {
-                    return(
-                        <div key={item.id} className="gallery">
-                            <h2 className="gallery__title">NEXT VIDEO</h2>
-                            <div className="gallery__container">
-                                <div className="gallery__video">{item.img}</div>
-                                <h3 className="gallery__title">{item.title}</h3>
-                                <h4 className="gallery__author">{item.author}</h4>
-                            </div>
-                        </div>
-                    )
-                })
-            );
-        }
-};  
+export default function Videos(props) {
+    return (
+        <div className="gallery">
+            <h2 className="gallery__title">NEXT VIDEO</h2>
+            <div className="gallery__container">
+                <video 
+                    className="gallery__video"
+                    src={props.videoSrc}
+                    alt={props.altText} 
+                />
+                <h3 className="gallery__title">{props.videoTitle}</h3>
+                <h4 className="gallery__author">{props.videoAuthor}</h4>
+            </div>
+        </div>
+    )
+}
 
-
-export default VideoGallery;
