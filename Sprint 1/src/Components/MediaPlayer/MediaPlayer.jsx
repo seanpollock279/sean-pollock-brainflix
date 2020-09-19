c43cadce1b8998202e4fdb1c6527c92bf4e2c59c
Sean Pollock <seanpollock@Seans-MacBook-Air-2.local>
2020-09-19 16:07:30 -0400

    mobile styling done

--- Sprint 1/src/Components/MediaPlayer/MediaPlayer.jsx
+++ Sprint 1/src/Components/MediaPlayer/MediaPlayer.jsx
@@ -7,21 +7,20 @@ import './Styles/MediaPlayer.scss';
 export default function MediaPlayer(props) {
     return (
         <div className="media">
-            <div>
+            <div className="media__containerTop">
                 <video className="media__video"
                 poster={props.videoSrc} 
                 alt={props.altText}
                 />
             </div>
-            <div className="media__container">
+            {/* <div className="media__containerIcons"> */}
                 <button className="media__leftButton"><img className="media__leftButtonIcon"src={Play} alt=""/></button>
                 <button className="media__centreButton">
                     <hr className="media__scrubber"/>
                     <p className="media__ticker">0:00/0:42</p>
                 </button>
-                <button className="media__rightButton"><img className="media__rightButtonIcon" src={FullScreen} alt=""/><img className="media__rightButtonIcon" src={Volume} alt=""/></button>
-            </div>
-            
+                <button className="media__rightButton"><img className="media__rightButtonIcon" src={FullScreen} alt=""/><img className="media__rightRightButtonIcon" src={Volume} alt=""/></button>
+            {/* </div> */}
         </div>
     )
 }
